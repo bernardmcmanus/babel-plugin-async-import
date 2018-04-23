e2ac1d81790f86dc1af8fec85cec4adc7289aae3
Bernard McManus <bernard.mcmanus.iv@gmail.com>
2018-04-23 15:16:45 -0400

    add support for custom templates, upgrade to babel 7

--- src/index.js
+++ src/index.js
@@ -1,68 +1,33 @@
-import Template from 'babel-template';
-import syntax from 'babel-plugin-syntax-dynamic-import';
-import * as t from 'babel-types';
+import Template from '@babel/template';
+import syntax from '@babel/plugin-syntax-dynamic-import';
 
-const template = Template('(() => CONDITION ? AFFIRMATIVE : NEGATIVE)()');
+export default (_, options) => {
+	const opts = {
+		template: undefined,
+		browser: 'import(REQUEST)',
+		node: 'Promise.resolve(require(REQUEST))',
+		syntax: ['asyncGenerators', 'dynamicImport', 'objectRestSpread'],
+		...options
+	};
 
-/**
- * (['a']) => a
- * (['a', 'b']) => a.b
- * (['a', 'b', 'c']) => a.b.c
- */
-function membersToExpression(members) {
-	return members.length === 1
-		? t.Identifier(members[0])
-		: t.MemberExpression(
-			membersToExpression(members.slice(0, -1)),
-			t.Identifier(...members.slice(-1))
-		)
-}
+	const history = new WeakSet();
+	const template = Template(
+		opts.template || opts[process.env.BABEL_TARGET] || opts.node,
+		{ plugins: opts.syntax }
+	);
 
-/**
- * (['a'], arg) => a(arg)
- * (['a', 'b', 'c'], arg) => a.b.c(arg)
- * (['a'], [undefined, ...args]) => a.apply(undefined, args)
- * (['a', 'b', 'c'], [undefined, ...args]) => a.b.c.apply(a.b, args)
- */
-function maybeApply(members, args) {
-	return args.length === 1
-		? [membersToExpression(members), args]
-		: [
-				membersToExpression([...members, 'apply']),
-				[
-					membersToExpression((members.length === 1 ? ['undefined'] : members.slice(0, -1))),
-					...args.slice(1)
-				]
-		];
-}
-
-export default () => ({
-	inherits: syntax,
-	visitor: {
-		Import(path) {
-			const targetPath = path.findParent(parentPath => parentPath.isCallExpression());
-			if (targetPath) {
+	return {
+		inherits: syntax,
+		visitor: {
+			Import(path) {
+				const targetPath = path.findParent(parentPath => parentPath.isCallExpression());
 				const { arguments: args } = targetPath.node;
-				if (args && args.length > 0) {
-					targetPath.replaceWith(
-						template({
-							CONDITION: membersToExpression(['process', 'browser']),
-							AFFIRMATIVE: t.ExpressionStatement(
-								t.CallExpression(
-									...maybeApply(['System', 'import'], args)
-								)
-							),
-							NEGATIVE: t.ExpressionStatement(
-								t.CallExpression(membersToExpression(['Promise', 'resolve']), [
-									t.CallExpression(
-										...maybeApply(['require'], args)
-									)
-								])
-							)
-						})
-					);
+				if (args && args.length > 0 && !history.has(targetPath.node)) {
+					const replacementNode = template({ REQUEST: args });
+					targetPath.replaceWith(replacementNode);
+					history.add(replacementNode.expression);
 				}
 			}
 		}
-	}
-});
+	};
+};
